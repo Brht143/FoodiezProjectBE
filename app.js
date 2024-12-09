b07c800f9fa8dbf1858f779538207581d208f805
KwSalem <167813808+KwSalem@users.noreply.github.com>
2024-12-09 20:13:32 +0300

    Add new code

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+// my new code
+app.get("/", (req, res) => {
+  res.json({ message: "Hello World!" });
+});
 app.listen(port, () =>
   console.log(`Express is online on http://192.168.2.108:${port}`)
 );
