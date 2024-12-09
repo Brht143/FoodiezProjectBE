670e973bfbc23bda88c4544d4650441549747eb5
Mohammed Almethen <almethen2002@gmail.com>
2024-12-09 19:41:48 +0300

    app.get

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.get("/", (req, res) => {
+  res.json({ name: "Mohammed" });
+});
 app.listen(port, () =>
   console.log(`Express is online on http://192.168.2.108:${port}`)
 );
